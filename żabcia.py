640e3717bbe7fe3fd493bc6f7ba89f5de5656dc2
tykfikk <104891802+tykfikk@users.noreply.github.com>
2022-06-12 18:45:55 +0200

    Update żabcia.py

--- żabcia.py
+++ żabcia.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import os
 from pygame import rect
+from random import randint
 
 
 
@@ -20,13 +21,11 @@ autko_odwrocone = pygame.image.load("auto2.png").convert_alpha()
 klody = pygame.image.load("kloda1.png").convert_alpha()
 zolwiki = pygame.image.load("zolwie1.png").convert_alpha()
 zolwiki_odwrocone = pygame.image.load("zolwie2.png").convert_alpha()
-liczba_zyc0 = pygame.image.load("serca0.png").convert_alpha()
-liczba_zyc1 = pygame.image.load("serca1.png").convert_alpha()
-liczba_zyc2 = pygame.image.load("serca2.png").convert_alpha()
-liczba_zyc3 = pygame.image.load("serca3.png").convert_alpha()
+zaba_win = pygame.image.load("zaba_win3.png").convert_alpha()
+zaba_ups = pygame.image.load("zaba_ups4.png").convert_alpha()
+zaba_GO = pygame.image.load("zaba_GameOver2.png").convert_alpha()
 
-
-
+### klasy (postać i obiekty) ###
 class Zaba():
     def __init__(self):
         self.zaba = postac.get_rect()
@@ -34,9 +33,7 @@ class Zaba():
         self.y_zaba = 560
         self.zaba_szerokosc = 40
         self.zaba_wysokosc = 40
-        # self.zaba_szybkosc = 
         self.zycie = 1
-   
 
     def ruch_postaci(self):
         ruch_zaby = pygame.key.get_pressed()
@@ -82,7 +79,7 @@ class Kloda():
         self.kloda_szerokosc = 240
         self.szybkosc = szybkosc
 
-    def ruch_klody(self): 
+    def ruch_klody(self):
         self.x_kloda -= self.szybkosc
         if self.x_kloda<-120:
             self.x_kloda = 600 
@@ -93,6 +90,7 @@ class Kloda():
         if self.x_kloda>720:
             self.x_kloda = -120
             self.y_kloda = self.y_kloda
+
 
 class Zolwie():
     def __init__(self, szybkosc, x, y):
@@ -115,38 +113,31 @@ class Zolwie():
             self.x_zolw = -120
             self.y_zolw = self.y_zolw 
 
+
+### funkcje ###
 def wykryj_kolizje(obiekt):
     if zabka.x_zaba >= obiekt.x_auto and (zabka.x_zaba+zabka.zaba_szerokosc)<=(obiekt.x_auto+obiekt.auto_szerokosc) and zabka.y_zaba<=obiekt.y_auto and (zabka.y_zaba-zabka.zaba_wysokosc)>=(obiekt.y_auto-obiekt.auto_wysokosc):
         return True
 
-# def kolizja(obiekt):
-#     if wykryj_kolizje(obiekt)==True:
-#         zabka.x_zaba = 280
-#         zabka.y_zaba = 560
+
 def kolizja(obiekt):
-    liczba_smierci = 0
     if wykryj_kolizje(obiekt)==True:
-        liczba_smierci = liczba_smierci + 1
-        if liczba_smierci <=2:
-            print(liczba_smierci)
-            okno.blit(zaba_ups, [0, 0])
-            pygame.display.update()
-            pygame.time.delay(2500)
-            zabka.x_zaba = 280
-            zabka.y_zaba = 560
-        elif liczba_smierci == 3:
-            print(liczba_smierci)
-            okno.blit(zaba_GO, [0, 0])
-            pygame.display.update()
-            pygame.time.delay(2500)
+        okno.blit(zaba_GO, [0,0])
+        pygame.display.update()
+        pygame.time.delay(2500)
+        zabka.x_zaba = 280
+        zabka.y_zaba = 560
+
 
 def wykryj_klody(obiekt):
     if zabka.x_zaba >= obiekt.x_kloda and (zabka.x_zaba+zabka.zaba_szerokosc)<=(obiekt.x_kloda+obiekt.kloda_szerokosc) and zabka.y_zaba<=obiekt.y_kloda and (zabka.y_zaba-zabka.zaba_wysokosc)>=(obiekt.y_kloda-obiekt.kloda_wysokosc):
         return True
 
+
 def wykryj_zolwie(obiekt):
     if zabka.x_zaba >= obiekt.x_zolw and (zabka.x_zaba+zabka.zaba_szerokosc)<=(obiekt.x_zolw+obiekt.zolw_szerokosc) and zabka.y_zaba<=obiekt.y_zolw and (zabka.y_zaba-zabka.zaba_wysokosc)>=(obiekt.y_zolw-obiekt.zolw_wysokosc):
         return True
+
 
 def zaba_na_klodach_prawo(obiekt,dana_szybkosc):
     if wykryj_klody(obiekt)==True:
@@ -162,36 +153,23 @@ def zaba_na_zolwiach_prawo(obiekt,dana_szybkosc):
     if wykryj_zolwie(obiekt)==True:
         zabka.x_zaba+=dana_szybkosc
 
+
 def zaba_na_zolwiach_lewo(obiekt,dana_szybkosc):
     if wykryj_zolwie(obiekt)==True:
         zabka.x_zaba-=dana_szybkosc
 
 
-
-# def zaba_na_wodzie():
-#     if zabka.x_zaba >= 0 and (zabka.x_zaba+zabka.zaba_szerokosc)<=600 and zabka.y_zaba<=280 and (zabka.y_zaba-zabka.zaba_wysokosc)>=120:
-#         return True
-#     else:
-#         return False 
-
-###trzeba zwoda polaczyc
-licznik_zyc=3
-for i in range (3):
-    if wykryj_kolizje or zaba_w_wodzie:
-        licznik_zyc-=1
-        if licznik_zyc==2:
-            okno.blit(liczba_zyc3, [-10,-10]
-            okno.blit(liczba_zyc2, [10, 0])
-        elif licznik_zyc==1:
-            okno.blit(liczba_zyc2,[-10,-10])
-            okno.blit(liczba_zyc1, [10,0])
-        elif licznik_zyc==0:
-            okno.blit(liczba_zyc1, [-10,-10])
-            okno.blit(liczba_zyc0, [10,0])
-            print("przegrałeś")
+def zaba_na_wodzie():
+    if zabka.x_zaba >= 0 and (zabka.x_zaba+zabka.zaba_szerokosc)<=600 and zabka.y_zaba<=280 and (zabka.y_zaba-zabka.zaba_wysokosc)>=80:
+        return True
 
 
+def win():
+    if zabka.y_zaba<=79:
+        return True
 
+
+### tworzenie obiektów danych klas ###
 zabka=Zaba()
 auto1=Auto(10,560,520)
 auto2=Auto(15,200,480)
@@ -200,15 +178,18 @@ auto4=Auto(16,700,400)
 auto5=Auto(13,300,360)
 kloda1=Kloda(9,230,280)
 kloda2=Kloda(11,-120,200)
-kloda3=Kloda(6,100,120)
+kloda3=Kloda(6,-400,120)
+kloda4=Kloda(6,0,120)
 zolwie1=Zolwie(8,890,240)
-zolwie2=Zolwie(13,500,160)
+zolwie2=Zolwie(9,150,160)
+zolwie3=Zolwie(8,300,240)
+zolwie4=Zolwie(9,500,160)
 
 
 ## jeden krok żabki i szerokość "pasów ruchu"
 postac_liczba_pikseli = 40
 
-
+### uruchamiamy grę ###
 running = True
 while running:
     for akcja in pygame.event.get():
@@ -228,6 +209,9 @@ while running:
     kloda2.ruch_klody()
     zolwie2.ruch_zolwia()
     kloda3.ruch_klody_prawo()
+    zolwie3.ruch_zolwia_prawo()
+    kloda4.ruch_klody_prawo()
+    zolwie4.ruch_zolwia()
     wykryj_kolizje(auto1)
     kolizja(auto1)
     wykryj_kolizje(auto2)
@@ -238,27 +222,40 @@ while running:
     kolizja(auto4)
     wykryj_kolizje(auto5)
     kolizja(auto5)
-    wykryj_klody(kloda1)
-    wykryj_klody(kloda2)
-    wykryj_klody(kloda3)
-    wykryj_zolwie(zolwie1)
-    wykryj_zolwie(zolwie2)
-    zaba_na_klodach_lewo(kloda1,9)
-    zaba_na_klodach_lewo(kloda2,11)
-    zaba_na_klodach_prawo(kloda3,6)
-    zaba_na_zolwiach_prawo(zolwie1,8)
-    zaba_na_zolwiach_lewo(zolwie2,13)
+    if zaba_na_wodzie()==True:
+        if wykryj_klody(kloda1)==True or wykryj_klody(kloda2)==True or wykryj_klody(kloda3)==True or wykryj_klody(kloda3)==True or wykryj_klody(kloda4)==True or wykryj_zolwie(zolwie1)==True or wykryj_zolwie(zolwie2)==True or wykryj_zolwie(zolwie3) or wykryj_zolwie(zolwie4)==True:
+            zaba_na_klodach_lewo(kloda1,9)
+            zaba_na_klodach_lewo(kloda2,11)
+            zaba_na_klodach_prawo(kloda3,6)
+            zaba_na_klodach_prawo(kloda4,6)
+            zaba_na_zolwiach_prawo(zolwie1,8)
+            zaba_na_zolwiach_lewo(zolwie2,13)
+            zaba_na_zolwiach_prawo(zolwie3,8)
+            zaba_na_zolwiach_lewo(zolwie4,9)
+        else: 
+            okno.blit(zaba_GO, [0,0])
+            pygame.display.update()
+            pygame.time.delay(2500)
+            zabka.x_zaba = 280
+            zabka.y_zaba = 560
+    if win()==True:
+        okno.blit(zaba_win, [0,0])
+        pygame.display.update()
+        pygame.time.delay(2500)
+        zabka.x_zaba = 280
+        zabka.y_zaba = 560
 
 
- 
-    # pygame.time.delay(60)
-
+### wstawianie grafik ###
     okno.blit(tlo_gry, [0, 0])
     okno.blit(klody, [kloda1.x_kloda, kloda1.y_kloda])
     okno.blit(klody, [kloda2.x_kloda, kloda2.y_kloda])
-    okno.blit(klody, [kloda3.x_kloda, kloda3.y_kloda])   
+    okno.blit(klody, [kloda3.x_kloda, kloda3.y_kloda])  
+    okno.blit(klody, [kloda4.x_kloda, kloda4.y_kloda]) 
     okno.blit(zolwiki_odwrocone, [zolwie1.x_zolw, zolwie1.y_zolw])
     okno.blit(zolwiki, [zolwie2.x_zolw, zolwie2.y_zolw])
+    okno.blit(zolwiki_odwrocone, [zolwie3.x_zolw, zolwie3.y_zolw])
+    okno.blit(zolwiki, [zolwie4.x_zolw, zolwie4.y_zolw])
     okno.blit(postac, [zabka.x_zaba, zabka.y_zaba])
     okno.blit(autko, [auto1.x_auto, auto1.y_auto])
     okno.blit(autko, [auto2.x_auto, auto2.y_auto])
